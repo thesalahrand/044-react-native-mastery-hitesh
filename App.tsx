c02d80f46271d7d3b32d182374b1440d40524928
Mohammad Salah <sourav.cse5.bu@gmail.com>
2023-11-21 19:08:35 +0600

    project_02: flat cards created

--- App.tsx
+++ App.tsx
@@ -1,29 +1,13 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, useColorScheme, Text} from 'react-native';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  whiteText: {
-    color: '#ffffff',
-  },
-  darkText: {
-    color: '#000000',
-  },
-});
+import FlatCards from './components/FlatCards';
+import {SafeAreaView, ScrollView} from 'react-native';
 
 const App = (): JSX.Element => {
-  const isDark = useColorScheme() === 'dark';
-
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={isDark ? styles.whiteText : styles.darkText}>
-        Hello World!
-      </Text>
-      {/* <Text style={styles.darkText}>Hello World!</Text> */}
+    <SafeAreaView>
+      <ScrollView>
+        <FlatCards />
+      </ScrollView>
     </SafeAreaView>
   );
 };
---
+++ components/FlatCards.tsx
@@ -0,0 +1,57 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 12,
+  },
+  headingText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#111827',
+    marginBottom: 12,
+  },
+  flatCardsWrapper: {
+    flex: 1,
+    flexDirection: 'row',
+    gap: 12,
+  },
+  flatCard: {
+    width: 120,
+    height: 120,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  flatCard01: {
+    backgroundColor: '#F05252',
+  },
+  flatCard02: {
+    backgroundColor: '#0E9F6E',
+  },
+  flatCard03: {
+    backgroundColor: '#3F83F8',
+  },
+  flatCardText: {
+    color: '#ffffff',
+  },
+});
+
+export default function FlatCards() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.headingText}>FlatBoxes</Text>
+      <View style={styles.flatCardsWrapper}>
+        <View style={[styles.flatCard, styles.flatCard01]}>
+          <Text style={styles.flatCardText}>Red</Text>
+        </View>
+        <View style={[styles.flatCard, styles.flatCard02]}>
+          <Text style={styles.flatCardText}>Green</Text>
+        </View>
+        <View style={[styles.flatCard, styles.flatCard03]}>
+          <Text style={styles.flatCardText}>Blue</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
