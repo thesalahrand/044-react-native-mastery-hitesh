000e764b9a0cade2d1641ad3950ac1e51c5c86fa
Mohammad Salah <sourav.cse5.bu@gmail.com>
2023-11-21 12:57:31 +0600

    feat: learnt to use stylesheet

--- App.tsx
+++ App.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
-import {SafeAreaView, View, Text} from 'react-native';
+import {SafeAreaView, StyleSheet, useColorScheme, Text} from 'react-native';
 
-const App = () => {
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  whiteText: {
+    color: '#ffffff',
+  },
+  darkText: {
+    color: '#000000',
+  },
+});
+
+const App = (): JSX.Element => {
+  const isDark = useColorScheme() === 'dark';
+
   return (
-    <SafeAreaView>
-      <View>
-        <Text>Hello World!</Text>
-      </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={isDark ? styles.whiteText : styles.darkText}>
+        Hello World!
+      </Text>
+      {/* <Text style={styles.darkText}>Hello World!</Text> */}
     </SafeAreaView>
   );
 };
